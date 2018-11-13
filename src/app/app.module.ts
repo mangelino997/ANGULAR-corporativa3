ec2f6dbe2c9aa5fdfcf3f8a8f810fa8379fa2b1d
Blas Bravo <blas@draimo.com>
2018-11-13 11:42:05 -0300

    Se agregó librerías a utilizar, se creó carpetas modelos, servicios y componentes, se agregó imports de librerías en app.module.ts y se creó url base en app.service.ts.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,48 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule, MatCheckboxModule, MatMenuModule, MatToolbarModule, MatDividerModule,
+  MatSelectModule, MatTabsModule, MatIconModule, MatCardModule, MatSidenavModule,
+  MatAutocompleteModule, MatInputModule, MatRadioModule, MatTableModule, MatDialogModule,
+  MatProgressBarModule } from '@angular/material';
+import { HttpModule } from '@angular/http';
+import { ToastrModule } from 'ngx-toastr';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule,
+    HttpModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatDividerModule,
+    MatSelectModule,
+    MatTabsModule,
+    MatIconModule,
+    MatCardModule,
+    MatSidenavModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    MatRadioModule,
+    MatTableModule,
+    MatDialogModule,
+    MatProgressBarModule,
+    ReactiveFormsModule.withConfig({warnOnNgModelWithFormControl: 'never'}),
+    ToastrModule.forRoot({
+      timeOut: 3000,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/servicios/app.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppService {
+
+  //Define la url base del servicio web
+  public URL_BASE:string = 'http://localhost:8080/hibana/auth';
+
+  //Constructor
+  constructor() { }
+
+  //Obtiene la url base
+  public getUrlBase():string {
+    return this.URL_BASE;
+  }
+
+}
